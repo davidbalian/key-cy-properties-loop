717c8b8f3e27cd0990f9b0d521202b5167b4383c
David Balyan <davidbalyan9@gmail.com>
2025-10-28 17:55:21 +0200

    Update SVG icon dimensions in favourites manager for consistent styling
    - Added width and height attributes to SVG icons in the favourites manager to ensure consistent sizing across different contexts.
    - This change enhances the visual appearance of the favourites icons, maintaining uniformity in the user interface.

--- includes/class-favourites-manager.php
+++ includes/class-favourites-manager.php
@@ -174,7 +174,7 @@ class KCPF_Favourites_Manager
     public static function getOutlineStarIcon()
     {
         return '<span class="kcpf-favourite-icon" aria-hidden="true">'
-            . '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">'
+            . '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20">'
             . '<path d="M480 208H308L256 48l-52 160H32l140 96-54 160 138-100 138 100-54-160z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></path>'
             . '</svg>'
             . '</span>';
@@ -188,8 +188,8 @@ class KCPF_Favourites_Manager
     public static function getFilledStarIcon()
     {
         return '<span class="kcpf-favourite-icon" aria-hidden="true">'
-            . '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">'
-            . '<path d="M394 480a16 16 0 01-9.39-3L256 383.76 127.39 477a16 16 0 01-24.55-18.08L153 310.35 23 221.2a16 16 0 019-29.2h160.38l48.4-148.95a16 16 0 0130.44 0l48.4 149H480a16 16 0 019.05 29.2L359 310.35l50.13 148.53A16 16 0 01394 480z"></path>'
+            . '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20">'
+            . '<path d="M394 480a16 16 0 01-9.39-3L256 383.76 127.39 477a16 16 0 01-24.55-18.08L153 310.35 23 221.2a16 16 0 019-29.2h160.38l48.4-148.95a16 16 0 0130.44 0l48.4 149H480a16 16 0 019.05 29.2L359 310.35l50.13 148.53A16 16 0 01394 480z" fill="currentColor"></path>'
             . '</svg>'
             . '</span>';
     }
