bc7a62cb4cb5809e97ccf4f4378610912cb15334
David Balyan <davidbalyan9@gmail.com>
2025-10-29 07:56:31 +0200

    increased max zoom

--- assets/js/map-view.js
+++ assets/js/map-view.js
@@ -81,7 +81,7 @@
           streetViewControl: false,
           fullscreenControl: true,
           zoomControl: true,
-          maxZoom: 12,
+          maxZoom: 14,
           mapId: "2fbf45e46e78d50c9d90cd84",
         }
       );
